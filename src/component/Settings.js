29137f265b91ed19a309f4cc4cfd04334f725c4f
Squashbucklr <2jbwar22@gmail.com>
2021-02-22 15:04:49 -0500

    arc order menu no longer open by default

--- src/component/Settings.js
+++ src/component/Settings.js
@@ -11,7 +11,7 @@ class Settings extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            showArcOrderSelector: true,
+            showArcOrderSelector: false,
             showFullArcOrder: false
         };
     }
